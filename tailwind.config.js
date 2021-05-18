70e26294a4ba76219599d2a0708720ab792cdc07
Yudha Nawawi <yudhanawawi12@gmail.com>
2021-05-19 00:08:13 +0700

    Docs: Install tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    darkMode: false, // or 'media' or 'class'
+    purge: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
