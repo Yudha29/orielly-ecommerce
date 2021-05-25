371e34714561cb59d44497a89e65d7993a38a727
Yudha Nawawi <yudhanawawi12@gmail.com>
2021-05-25 14:46:59 +0700

    Docs: Add input component

---
+++ resources/views/components/form/input.blade.php
@@ -0,0 +1,16 @@
+<div class="mb-3">
+    <label class="text-gray-500 block font-bold text-xs mb-1.5">{{ $label }}</label>
+    <input
+        class="border-gray-300 rounded-md focus:border focus:outline-none text-sm placeholder-gray-400 text-gray-700 border py-2.5 px-2 w-full"
+        placeholder="{{ $placeholder }}"
+        type="{{ $type }}"
+        name="{{ $name }}"
+        value="{{ $value }}"
+    >
+    @if ($error)
+        <span class="block mt-0.5 text-xs text-right text-red-500">
+            <i class="fa fa-exclamation-circle"></i>
+            {{ $error }}
+        </span>
+    @endif
+</div>
