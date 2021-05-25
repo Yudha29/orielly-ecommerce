5d054aebc47db89d824946233a8e2bb70db5f555
Yudha Nawawi <yudhanawawi12@gmail.com>
2021-05-25 15:11:23 +0700

    Docs: Add basic layout component

---
+++ resources/views/components/basic-layout.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
+
+    <title>{{ $title }}</title>
+</head>
+
+<body data-new-gr-c-s-check-loaded="14.1011.0" data-gr-ext-installed="">
+    {{ $slot }}
+<!--
+  This HTML file is a template.
+  If you open it directly in the browser, you will see an empty page.
+
+  You can add webfonts, meta tags, or analytics to this file.
+  The build step will place the bundled scripts into the <body> tag.
+
+  To begin the development, run `npm start` or `yarn start`.
+  To create a production bundle, use `npm run build` or `yarn build`.
+-->
+
+<script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
