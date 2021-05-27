d50c7a6ac693b70e837324289080ce5754a5b849
Yudha Nawawi <yudhanawawi12@gmail.com>
2021-05-27 10:52:10 +0700

    Fix (View.Layout): Make script not required

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -152,6 +152,6 @@
     -->
     <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
     <script src="{{asset('js/app.js')}}"></script>
-    {{ $script }}
+    {{ $script ?? '' }}
 </body>
 </html>
