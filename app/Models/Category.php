c2fc49c442e704f66dc6206dcf9e2dccbc70b5c7
Yudha Nawawi <yudhanawawi12@gmail.com>
2021-06-10 10:12:32 +0700

    Docs: Make category model auto generate UUID for id field

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use App\Traits\Uuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
+    use Uuids;
     use HasFactory;
     use SoftDeletes;
 
